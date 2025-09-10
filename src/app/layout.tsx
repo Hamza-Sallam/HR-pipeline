5327db009532af84e9661389a3eea8b848cb58c6
hamza <hamza.sallam.dev@outlook.com>
2025-09-11 01:45:24 +0300

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { LanguageProvider } from '@/contexts/language-context';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'TalentFlow',
@@ -32,6 +33,7 @@ export default function RootLayout({
             <Toaster />
           </LanguageProvider>
         </Suspense>
+        <Analytics />
       </body>
     </html>
   );
